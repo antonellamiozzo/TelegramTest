c3193ffb89b79292bab80068b0060fbda26f48e5
antonellamiozzo <antonella.miozzo@hotmail.com>
2018-09-06 13:30:03 -0300

    fsf

--- BotAnto/src/main/java/AntoBot.java
+++ BotAnto/src/main/java/AntoBot.java
@@ -1,5 +1,8 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -8,7 +11,32 @@ public class AntoBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
 
-        System.out.println(update.getMessage().getText());
+        //System.out.println(update.getMessage().getFrom().getFirstName());
+       // System.out.println(update.getMessage().getText());
+
+        String command = update.getMessage().getText();
+        SendMessage message = new SendMessage();
+
+        if(command.equals("/name")){
+            System.out.println(update.getMessage().getFrom().getFirstName());
+            message.setText(update.getMessage().getFrom().getFirstName());
+        }
+        if(command.equals("/lastname")){
+            System.out.println(update.getMessage().getFrom().getLastName());
+            message.setText(update.getMessage().getFrom().getLastName());
+        }
+        if(command.equals("/fullname")){
+            System.out.println(update.getMessage().getFrom().getFirstName()+" "+ update.getMessage().getFrom().getLastName());
+            message.setText(update.getMessage().getFrom().getFirstName()+" "+ update.getMessage().getFrom().getLastName());
+        }
+
+        message.setChatId(update.getMessage().getChatId());
+
+        try {
+            execute(message);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
 
     }
 
