e5a85bdc90d8880eac88961e0dff76ed5e78bc7d
antonellamiozzo <antonella.miozzo@hotmail.com>
2018-10-08 16:55:27 -0300

    all

--- BotAnto/src/main/java/AntoBot.java
+++ BotAnto/src/main/java/AntoBot.java
@@ -4,17 +4,20 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class AntoBot extends TelegramLongPollingBot {
     static String BotToken = "625563171:AAFyoxqMiAua2gLEGVRYcYF00KhAa2aYyG0";
     long ChatID ;
+    boolean LOGIN = false;
 
+    private String username = "Username";
+    private String password = "Password";
 
     public void onUpdateReceived(Update update) {
-        String username = "Username";
-        String password ="Password" ;
 
         long eventID =0;
 
@@ -25,40 +28,67 @@ public class AntoBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         Integer id = update.getUpdateId();
 
-        if (command.contains("/buscarEvento")){
-            MandarMensaje(update.getMessage().getText());
-        }
         if(command.equals("/login")) {
-            if((username.equals("Username")) || (password.equals("Password"))  )
+            if((username.equals("Username")) || (password.equals("Password"))  ) {
+                System.out.println(username + " " + password);
+
                 MandarMensaje("Please set your username and password to login");
-            else{
+            } else{
             //llamo al metodo de login
+
+                LOGIN = true;
+                System.out.println(username + " " + password);
             MandarMensaje("Se loguea bebe");
             MandarMensaje("Login Successfully");
             }
         }
 
-        if(command.equals("/setusername")) {
-            System.out.println(update.getMessage().getText());
-            username= update.getMessage().getText();
-            MandarMensaje("Ok");
+        if(command.contains("/start")){
+
+            MandarMensaje("Bienvenido, ingrese su usuario y contraseña para poder loguearse y acceder a los servicios de EventManager. \n" +
+                    "Los comandos disponibles son :\n" +
+                    "login - Loguearse\n" +
+                    "username - Setear Username para loguerse\n" +
+                    "password - Setear Password para loguearse\n" +
+                    "buscarevento - Buscar Evento\n" +
+                    "agregarevento - Agregar evento\n" +
+                    "revisarevento - Revisar evento\n" +
+                    "resetear - Volver a loguerse con otra cuenta ");
         }
-        if(command.equals("/setpassword")) {
+
+        if(command.contains("/username")) {
+            List <String> parametros = ParsearComando(command);
             System.out.println(update.getMessage().getText());
-            password= update.getMessage().getText();
+            username= parametros.get(0);
+
+            System.out.println(username);
+           // username=parametros.get(0);
+            MandarMensaje("Ok " + parametros.get(0));
+        }
+        if(command.contains("/password")) {
+            List <String> parametros = ParsearComando(command);
+            System.out.println(update.getMessage().getText());
+           // password.replace(password,parametros.get(0));
+            password= parametros.get(0);
+            MandarMensaje("Ok");
         }
 
 
         if(command.contains("/buscarevento")){
-            List <String> parametros = ParsearComando(command);
+
+            if(LOGIN){
+           List <String> parametros = ParsearComando(command);
 
             if(parametros.size() != 0){
                 Long eventId = Long.parseLong(parametros.get(0));
                 MandarMensaje(parametros.get(0));
             }else {
                 MandarMensaje("Falta un parametros");
-            }
+            }}else
+                MandarMensaje("Debe loguearse primero");
         }
+
+
 
 
         if(command.contains("/agregarevento")){
@@ -119,4 +149,6 @@ public class AntoBot extends TelegramLongPollingBot {
 
         return parametros;
     }
+
+
 }
